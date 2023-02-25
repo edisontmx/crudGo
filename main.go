6c36153c195069dcc9250c09f6366d92d2ecd9f3
edisontmx <edison.mc@gmail.com>
2023-02-24 21:21:51 -0600

    compruebo conexion a la base de datos y agrego un registro desde el codigo

--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func conexionDB() (*sql.DB, error) {
+	connString := "root:@tcp(localhost:3308)/sistema?parseTime=true"
+
+	db, err := sql.Open("mysql", connString)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
+}
 
 var plantillas = template.Must(template.ParseGlob("plantillas/*"))
 
 func main() {
+
+	ctx := context.Background()
+	db, err := conexionDB()
+	if err != nil {
+		panic(err)
+	}
+
+	err = insert(ctx, db, "abiud Medina", "correoabiud@correo.com")
+
 	http.HandleFunc("/", index)
 	http.HandleFunc("/crear", Crear)
 
@@ -16,6 +47,7 @@ func main() {
 
 	http.ListenAndServe(":8080", nil)
 
+	db.Close()
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -25,4 +57,22 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func Crear(w http.ResponseWriter, r *http.Request) {
 	plantillas.ExecuteTemplate(w, "crear", nil)
+}
+
+func insert(ctx context.Context, db *sql.DB, nombre string, correo string) error {
+
+	queryADD := `INSERT INTO empleados( nombre, correo) values(?, ?)`
+
+	result, err := db.ExecContext(ctx, queryADD, nombre, correo)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	fmt.Println("inserted id: ", id)
+
+	return nil
 }
